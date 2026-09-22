37424e0882df318e376b460e6b2c00031cc8f165
Ewans Angel LS <ewansangel2005@gmail.com>
2026-09-22 10:22:43 +0530

    Add files via upload

---
+++ NonPara.java
@@ -0,0 +1,17 @@
+package oops;
+
+public class NonPara{
+
+    String name;
+
+    // Constructor
+    NonPara() {
+        System.out.println("Object is Created");
+    }
+
+    public static void main(String[] args) {
+
+        NonPara s = new NonPara();
+
+    }
+}
